c967a104ba3d11aa6a26e06bdb506a572927f67b
Greg Steinbrecher <steinbrecher@alum.mit.edu>
2019-01-26 13:20:08 -0800

    Fixed install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,17 +2,17 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]
 then
-    brew install gcc
+    { brew install --upgrade gcc || true; }
     case "${TOXENV}" in
 	py27)
-	    { brew install upgrade python@2 || true }
+	    { brew install --upgrade python@2 || true; }
 	    ;;
 	py37)
-	    { brew install upgrade python || true }
+	    { brew install --upgrade python || true; }
 	    ;;
     esac
     
-    CC=$(which gcc) pip install .
+    CC=$(which gcc-8) pip install .
 else
     pip install Cython
     pip install nose
